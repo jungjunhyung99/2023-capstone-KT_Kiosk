41b71036c02ff55484b034f62d8894c4304eabaf
jaryeong <dawntopjr@naver.com>
2023-05-05 16:43:10 +0900

    icecream__kiosk

---
+++ src/practice/Icecream/Icecream_cone.tsx
@@ -0,0 +1,443 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { ImageBox_IcecreamCone } from '../../component/kiosk-component/styled_kiosk';
+import Gugucone from "../../images/icecream/Gugucone.png";
+import Pigcone from "../../images/icecream/Pigcone.png";
+import Worldcone from "../../images/icecream/Worldcone.png";
+import Boorabo from "../../images/icecream/Boorabo.png";
+import { useState, useCallback } from 'react';
+import Modal from "../../component/Modal";
+
+function Icecream_cone() {
+    
+    const navigate = useNavigate();
+
+    const onClickIcecreamBar = () => {
+        navigate('/Icecream_bar');
+    }
+
+    const onClickIcecreamTube = () => {
+        navigate('/Icecream_tube');
+    }
+
+    const onClickIcecreamFamily = () => {
+        navigate('/Icecream_family');
+    }
+
+    const [number, setNumber] = useState(0);
+    const [number2, setNumber2] = useState(0);
+    const [number3, setNumber3] = useState(0);
+    const [number4, setNumber4] = useState(0);
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
+    const [count4, setCount4] = useState(0);
+
+    const btn_first_click = () => {
+        setNumber(number + 1);
+    }
+
+    const btn_second_click = () => {
+        setNumber2(number2 + 1);
+    }
+    
+    const btn_third_click = () => {
+        setNumber3(number3 + 1);
+    }
+
+    const btn_fourth_click = () => {
+        setNumber4(number4 + 1);
+    }
+
+    const btn_first_minus_click = () => {
+        setNumber(number - 1);
+    }
+
+    const btn_second_minus_click = () => {
+        setNumber2(number2 - 1);
+    }
+
+    const btn_third_minus_click = () => {
+        setNumber3(number3 - 1);
+    }
+
+    const btn_fourth_minus_click = () => {
+        setNumber4(number4 - 1);
+    }
+
+    const Count = () => {
+        setCount(count + 1);
+    }
+
+    const Count2 = () => {
+        setCount2(count2 + 1);
+    }
+
+    const Count3 = () => {
+        setCount3(count3 + 1);
+    }
+
+    const Count4 = () => {
+        setCount4(count4 + 1);
+    }
+
+    const Minus_Count = () => {    
+        setMinusCount(minus_count - 1);
+    }
+
+    const Minus_Count2 = () => {
+        setMinusCount2(minus_count2 - 1);
+    }
+
+    const Minus_Count3 = () => {
+        setMinusCount3(minus_count3 - 1);
+    }
+
+    const Minus_Count4 = () => {
+        setMinusCount4(minus_count4 - 1);
+    }
+
+    const [minus_count, setMinusCount] = useState(0);
+    const [minus_count2, setMinusCount2] = useState(0);
+    const [minus_count3, setMinusCount3] = useState(0);
+    const [minus_count4, setMinusCount4] = useState(0);
+    const [isOpenModal, setOpenModal] = useState<boolean>(false);
+
+    const onClickToggleModal = useCallback(() => {
+        setOpenModal(!isOpenModal);
+    }, [isOpenModal]);
+
+    const Title = styled.div`
+        width: 80vw;
+        font-size: 45px;
+        display: flex;
+        justify-content: center;
+        background-color: #CCA63D;
+        height: 100px;
+        font-style: italic;
+        align-items: center;
+        `;
+
+    const List = styled.div`
+        width: 80vw;
+        height: 80px;
+        display: flex;
+        background-color: #FFE08C;
+        `;
+
+    const ConeButton = styled.button`
+        font-size: 30px;
+        background-color: #FFE08C;
+        &:hover {
+            background-color: lightyellow;
+        }
+        width: 25%;
+        `;
+
+    const BarButton = styled.button`
+        font-size: 30px;
+        background-color: #FFE08C;
+        &:hover {
+            background-color: lightyellow;
+        }
+        width: 25%;
+    `;
+
+    const TubeButton = styled.button`
+        font-size: 30px;
+        background-color: #FFE08C;
+        &:hover {
+            background-color: lightyellow;
+        }
+        width: 25%;
+    `;
+
+    const FamilyButton = styled.button`
+        font-size: 30px;
+        background-color: #FFE08C;
+        &:hover {
+            background-color: lightyellow;
+        }
+        width: 25%;
+    `;
+
+    const Warning = styled.div`
+        font-size: 30px;
+        font-weight: bold;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        height: 100px;
+        `;
+
+    const Items = styled.div`
+        display: flex;
+        justify-content: center;
+    `;
+
+    const GuguconeBox = styled.button`
+        background-color: white;
+        border-width: 1px;
+        position: relative;
+        right: 12.5rem;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const PigconeBox = styled.button`
+        background-color: white;
+        border-width: 1px;
+        position: relative;
+        right: 5.2em;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const WorldconeBox = styled.button`
+        background-color: white;
+        border-width: 1px;
+        position: relative;
+        left: 4.3rem;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const BooraboBox = styled.button`
+        background-color: white;
+        border-width: 1px;
+        position: relative;
+        left: 12.6rem;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const GuguconePrice = styled.div`
+        font-size: 30px;
+    `;
+
+    const PigconePrice = styled.div`
+        font-size: 30px;
+    `
+
+    const WorldconePrice = styled.div`
+        font-size: 30px;
+    `;
+
+    const BooraboPrice = styled.div`
+        font-size: 30px;
+    `;
+
+    const MinusDivButton = styled.div`
+        display: flex;
+        justify-content: center;
+    `;
+
+    const FirstButton = styled.button`
+        width: 60px;
+        height: 50px;
+        background-color: green;
+        border-width: 1px;
+        position: relative;
+        top: 10px;
+        right: 315px;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const SecondButton = styled.button`
+        width: 60px;
+        height: 50px;
+        background-color: green;
+        border-width: 1px;
+        position: relative;
+        top: 10px;
+        right: 70px;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const ThirdButton = styled.button`
+        width: 60px;
+        height: 50px;
+        background-color: green;
+        border-width: 1px;
+        position: relative;
+        top: 10px;
+        left: 183px;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const FourthButton = styled.button`
+        width: 60px;
+        height: 50px;
+        background-color: green;
+        border-width: 1px;
+        position: relative;
+        top: 10px;
+        left: 430px;
+        &:hover {
+            background-color: lightyellow;
+        }
+    `;
+
+    const FirstButtonDiv = styled.div`
+        color: white;
+        font-size: 40px;
+        display: flex;
+        justify-content: center;
+    `;
+
+    const SecondButtonDiv = styled.div`
+        color: white;
+        font-size: 40px;
+        display: flex;
+        justify-content: center;
+    `;
+
+    const ThirdButtonDiv = styled.div`
+        color: white;
+        font-size: 40px;
+        display: flex;
+        justify-content: center;
+    `;
+
+    const FourthButtonDiv = styled.div`
+        color: white;
+        font-size: 40px;
+        display: flex;
+        justify-content: center;
+    `;
+ 
+    const CountItems = styled.div`
+        display: flex;
+        justify-content: center;
+        font-size: 90px;
+    `;
+
+    const FirstCount = styled.div`
+        position: relative;
+        right: 400px;
+    `;
+
+    const SecondCount = styled.div`
+        position: relative;
+        right: 130px;
+    `;
+
+    const ThirdCount = styled.div`
+        position: relative;
+        left: 130px;
+    `;
+
+    const FourthCount = styled.div`
+        position: relative;
+        left: 390px;
+    `;
+
+    const Order = styled.button`
+        width: 80vw;
+        font-size: 30px;
+        background-color: red;
+        border-width: 1px;
+        height: 100px;
+        font-color: white;
+    `;
+
+    const OrderDiv = styled.div`
+        width: 80vw;
+        color: white;
+    `;
+
+    const GuguText = styled.div`
+        font-size: 30px;
+        position: relative;
+        top: 40px;
+    `;
+
+    const PigText = styled.div`
+        font-size: 30px;
+        position: relative;
+        top: 40px;
+    `;
+
+    const WorldText = styled.div`
+        font-size: 30px;
+        position: relative;
+        top: 40px;
+    `;
+
+    const BooraboText = styled.div`
+        font-size: 30px;
+        position: relative;
+        top: 40px;
+    `;
+
+    const Total = styled.div`
+        font-size: 30px;
+        position: relative;
+        top: 70px;
+    `;
+
+    const Close = styled.button`
+        font-size: 30px;
+        width: 100px;
+        position: relative;
+        top: 100px;
+        background-color: lightblue;
+    `;
+
+    return(
+        <div>
+            <Title>Icecream</Title>
+            <List>
+                <ConeButton>Cone</ConeButton>
+                <BarButton onClick={onClickIcecreamBar}>Bar</BarButton>
+                <TubeButton onClick={onClickIcecreamTube}>Tube</TubeButton>
+                <FamilyButton onClick={onClickIcecreamFamily}>Family</FamilyButton>
+            </List>
+            <Warning>주의 : 수량이 0보다 크도록 선택하세요. 아니면 주문에 실패합니다.</Warning>
+            <Items>
+                <GuguconeBox onClick={() => {btn_first_click(); Count()}}><ImageBox_IcecreamCone image={Gugucone}/><GuguconePrice>1,800원</GuguconePrice></GuguconeBox>
+                <PigconeBox onClick={() => {btn_second_click(); Count2()}}><ImageBox_IcecreamCone image={Pigcone}/><PigconePrice>2,200원</PigconePrice></PigconeBox>
+                <WorldconeBox onClick={() => {btn_third_click(); Count3()}}><ImageBox_IcecreamCone image={Worldcone}/><WorldconePrice>2,500원</WorldconePrice></WorldconeBox>
+                <BooraboBox onClick={() => {btn_fourth_click(); Count4()}}><ImageBox_IcecreamCone image={Boorabo}/><BooraboPrice>3,500원</BooraboPrice></BooraboBox>
+            </Items>
+            <MinusDivButton>
+                <FirstButton onClick={() => {btn_first_minus_click(); Minus_Count();}}><FirstButtonDiv>-</FirstButtonDiv></FirstButton>
+                <SecondButton onClick={() => {btn_second_minus_click(); Minus_Count2();}}><SecondButtonDiv>-</SecondButtonDiv></SecondButton>
+                <ThirdButton onClick={() => {btn_third_minus_click(); Minus_Count3();}}><ThirdButtonDiv>-</ThirdButtonDiv></ThirdButton>
+                <FourthButton onClick={() => {btn_fourth_minus_click(); Minus_Count4();}}><FourthButtonDiv>-</FourthButtonDiv></FourthButton>
+            </MinusDivButton>
+            <CountItems>
+                <FirstCount>{number}</FirstCount>
+                <SecondCount>{number2}</SecondCount>
+                <ThirdCount>{number3}</ThirdCount>
+                <FourthCount>{number4}</FourthCount>
+            </CountItems>
+            <Order onClick={onClickToggleModal}><OrderDiv>주문하기</OrderDiv></Order>
+            {isOpenModal && (
+                <Modal onClickToggleModal={onClickToggleModal}>
+                   <GuguText>더블콘 : 2,500원 * {count+(minus_count)}개 = {1800*(count+(minus_count))}원</GuguText>
+                            <PigText>돼지콘 : 2,200원 * {count2+(minus_count2)}개 = {2200*(count2+(minus_count2))}원</PigText>
+                            <WorldText>월드콘 : 2,500원 * {count3+(minus_count3)}개 = {2500*(count3+(minus_count3))}원</WorldText>
+                            <BooraboText>요맘때 : 3,500원 * {count4+(minus_count4)}개 = {3500*(count4+(minus_count4))}원</BooraboText> <br />
+                            <Total>총 금액 : {(2500*(count+(minus_count))
+                            +(2200*(count2+(minus_count2)))
+                            +(2500*(count3+(minus_count3)))
+                            +(3500*(count4+(minus_count4))))}원</Total>
+                            <Close onClick={() => setOpenModal(false)}>닫기</Close>
+                </Modal>
+            )}
+        </div>
+    );
+}
+
+export default Icecream_cone;
