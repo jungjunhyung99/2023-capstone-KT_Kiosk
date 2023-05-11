0a4fc8c592f37a7a4159e6d0208d485e1333186d
jaryeong <dawntopjr@naver.com>
2023-05-11 15:36:39 +0900

    conflict

--- src/component/kiosk-component/styled_kiosk.ts
+++ src/component/kiosk-component/styled_kiosk.ts
@@ -26,53 +26,6 @@ export const ImageBox = styled.div<{image: any}>`
     background-size: cover;
 `;
 
-export const ImageBox1 = styled.div<{image: any}>`
-    width: 9rem;
-    height: 9rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`
-export const ImageBox_IcecreamCone = styled.div<{image: any}>`
-    width: 10rem;
-    height: 22rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`
-export const ImageBox_IcecreamBar = styled.div<{image: any}>`
-    width: 18rem;
-    height: 8rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`;
-
-export const ImageBox_IcecreamTube = styled.div<{image: any}>`
-    width: 20rem;
-    height: 8rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`;
-
-export const ImageBox_IcecreamFamily = styled.div<{image: any}>`
-    width: 20rem;
-    height: 18rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`;
-
-export const ImageBox_IcecreamFamilyNattur = styled.div<{image: any}>`
-    width: 20rem;
-    height: 20rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`;
-
-export const ImageBox_IcecreamFamilyGoogoo = styled.div<{image: any}>`
-    width: 20rem;
-    height: 22.5rem;
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-`;
-
 export const KioskContainer = styled.div`
     display: flex;
     flex-direction: column;
